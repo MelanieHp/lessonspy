25035e7b6ce38fd474b4590d1e37d38d7b38da01
MelanieHp <mln.hp.ph@icloud.com>
2021-03-30 22:05:28 +0300

    Have added 3 new functions

--- codewarstasks.py
+++ codewarstasks.py
@@ -1,3 +1,6 @@
+
+# function that generates a phone number of a list.
+
 def number(q):
     for el in q:
         if el > 10 or len(q) != 10:
@@ -11,3 +14,17 @@ def number(q):
 
 q = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 print(number(q))
+
+# function that changes the letters in str.
+
+def DNA_strand(dna):
+    dict = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    return ''.join(dict[l] for l in dna if l in dict.keys())
+print(DNA_strand('ATCG'))
+
+# function that returns the sum of two least numbers.
+def sum_two_smallest_numbers(numbers):
+    numbers.sort()
+    return numbers[0] + numbers[1]
+numbers = [1, 10, 2, 6, 8]
+print(sum_two_smallest_numbers(numbers))
