9c58dedbe20024173f8af52ef7837bb1b29b6285
MelanieHp <mln.hp.ph@icloud.com>
2021-03-31 19:07:28 +0300

    Have added 3 new functions

--- codewarstasks.py
+++ codewarstasks.py
@@ -28,3 +28,42 @@ def sum_two_smallest_numbers(numbers):
     return numbers[0] + numbers[1]
 numbers = [1, 10, 2, 6, 8]
 print(sum_two_smallest_numbers(numbers))
+
+# ф-я которая определяет является ли число полным квадратом.
+
+from math import sqrt
+def is_square(n):
+    if n < 0:
+        return False
+
+    elif sqrt(n) == int(sqrt(n)):
+        return True
+
+    else:
+        return False
+
+print(is_square(12))
+
+# ф-я, кооторая находит следующий целочисленный полный квадрат после переданного в качестве параметра.
+from math import sqrt
+
+def find_next_square(sq):
+    if sqrt(sq) == int(sqrt(sq)):
+        sq_next = (sqrt(sq) + 1) ** 2
+
+        return int(sq_next)
+
+    else:
+        return -1
+
+print(find_next_square(144))
+
+# ф-я, которая принимает число и меняет его порядок от большего к меньшему.
+def descending_order(num):
+    str_num = str(num)
+    list_num = [int(n) for n in str_num]
+    list_num.sort(reverse=True)
+    num_1 = int(''.join(list(map(str, list_num))))
+    return num_1
+
+print(descending_order(34267))
